c1131a280f9c10dac27ceec11aa3b86bb0ad72ce
PrakashSingh65 <pratapsing5656@gmail.com>
2026-02-16 13:42:15 +0530

    Refactor Nav component layout and styling for improved responsiveness

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,17 +5,21 @@ import { FaBagShopping } from "react-icons/fa6";
 
 const Nav = () => {
   return (
-    <div className='w-full h-[100px] bg-red-200'>
-        <div className='w-60px h-60px rounded-full flex items-center justify-center text-3xl text-white'>
-            <MdFastfood />
+    <div className='w-full h-[100px] flex justify-between items-center px-8'>
+        <div className='w-[60px] h-[60px] bg-white flex justify-center
+         items-center rounded-md shadow-xl'>
+            <MdFastfood className='w-[30px] h-[30px] text-green-500' />
         </div>
-        <from action="">
+        <form className='w-[60%] h-[60px] bg-white flex
+        items-center px-5 ga-5'>
           <IoSearch />
-          <input type="text" placeholder='search Items...' />
-        </from>
-        <div className='w-full h-[100px] bg-red-200'>
-          <div className='w-60px h-60px rounded-full flex items-center justify-center text-3xl text-white'>
-            <FaBagShopping />
+          <input type="text" placeholder='search Items...'
+          className='w-[100%] outline-none text-[20px]' />
+        </form>
+          <div className='h-[100px]  flex items-center'>
+            <div className='w-[60px] h-[60px] bg-white flex justify-center 
+            items-center rounded-md shadow-xl'>
+            <FaBagShopping className='w-[30px] h-[30px] text-green-500' />
           </div>
         </div>
 
